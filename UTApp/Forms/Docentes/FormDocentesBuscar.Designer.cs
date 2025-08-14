8bd3844d2735134eca3b0e9325bee9b7c9ab2ffd
Paúl Ernesto López Peña <pelp.uts@gmail.com>
2025-08-13 20:42:19 -0700

    Se agregan pantallas de búsqueda en CRUD Estudiantes y Docentes

---
+++ UTApp/Forms/Docentes/FormDocentesBuscar.Designer.cs
@@ -0,0 +1,307 @@
+﻿namespace UTApp.Forms.Docentes
+{
+    partial class FormDocentesBuscar
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormDocentesBuscar));
+            this.panelHeader = new System.Windows.Forms.Panel();
+            this.labelMaterias = new System.Windows.Forms.Label();
+            this.labelCatálogoMaterias = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtDocenteEmpleado = new System.Windows.Forms.TextBox();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtDocentePass = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtDocenteCorreo = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtDocenteNombre = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtDocenteApellido = new System.Windows.Forms.TextBox();
+            this.labelIIngresarValores = new System.Windows.Forms.Label();
+            this.botonOpciones = new System.Windows.Forms.PictureBox();
+            this.pbRegresar = new System.Windows.Forms.PictureBox();
+            this.panelHeader.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.botonOpciones)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbRegresar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panelHeader
+            // 
+            this.panelHeader.BackColor = System.Drawing.Color.SeaGreen;
+            this.panelHeader.Controls.Add(this.botonOpciones);
+            this.panelHeader.Controls.Add(this.pbRegresar);
+            this.panelHeader.Controls.Add(this.labelMaterias);
+            this.panelHeader.Location = new System.Drawing.Point(1, -2);
+            this.panelHeader.Name = "panelHeader";
+            this.panelHeader.Size = new System.Drawing.Size(343, 94);
+            this.panelHeader.TabIndex = 21;
+            // 
+            // labelMaterias
+            // 
+            this.labelMaterias.AutoSize = true;
+            this.labelMaterias.BackColor = System.Drawing.Color.Transparent;
+            this.labelMaterias.Font = new System.Drawing.Font("Verdana", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelMaterias.ForeColor = System.Drawing.Color.White;
+            this.labelMaterias.Location = new System.Drawing.Point(119, 45);
+            this.labelMaterias.Name = "labelMaterias";
+            this.labelMaterias.Size = new System.Drawing.Size(110, 25);
+            this.labelMaterias.TabIndex = 0;
+            this.labelMaterias.Text = "Docentes";
+            // 
+            // labelCatálogoMaterias
+            // 
+            this.labelCatálogoMaterias.AutoSize = true;
+            this.labelCatálogoMaterias.Font = new System.Drawing.Font("Verdana", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelCatálogoMaterias.ForeColor = System.Drawing.Color.Black;
+            this.labelCatálogoMaterias.Location = new System.Drawing.Point(20, 120);
+            this.labelCatálogoMaterias.Name = "labelCatálogoMaterias";
+            this.labelCatálogoMaterias.Size = new System.Drawing.Size(108, 23);
+            this.labelCatálogoMaterias.TabIndex = 105;
+            this.labelCatálogoMaterias.Text = "Buscador";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(31, 177);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(97, 16);
+            this.label2.TabIndex = 136;
+            this.label2.Text = "No. Empleado:";
+            // 
+            // txtDocenteEmpleado
+            // 
+            this.txtDocenteEmpleado.Enabled = false;
+            this.txtDocenteEmpleado.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.249999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDocenteEmpleado.Location = new System.Drawing.Point(133, 176);
+            this.txtDocenteEmpleado.Name = "txtDocenteEmpleado";
+            this.txtDocenteEmpleado.Size = new System.Drawing.Size(118, 20);
+            this.txtDocenteEmpleado.TabIndex = 135;
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.BackColor = System.Drawing.Color.SeaGreen;
+            this.btnBuscar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnBuscar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBuscar.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnBuscar.Location = new System.Drawing.Point(94, 230);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(157, 38);
+            this.btnBuscar.TabIndex = 138;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(14, 422);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(114, 16);
+            this.label1.TabIndex = 149;
+            this.label1.Text = "Título académico:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Enabled = false;
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.249999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(133, 421);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(180, 20);
+            this.textBox2.TabIndex = 148;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(49, 501);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(79, 16);
+            this.label7.TabIndex = 147;
+            this.label7.Text = "Contraseña:";
+            // 
+            // txtDocentePass
+            // 
+            this.txtDocentePass.Enabled = false;
+            this.txtDocentePass.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.249999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDocentePass.Location = new System.Drawing.Point(133, 500);
+            this.txtDocentePass.Name = "txtDocentePass";
+            this.txtDocentePass.Size = new System.Drawing.Size(180, 20);
+            this.txtDocentePass.TabIndex = 146;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(77, 462);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(51, 16);
+            this.label5.TabIndex = 145;
+            this.label5.Text = "Correo:";
+            // 
+            // txtDocenteCorreo
+            // 
+            this.txtDocenteCorreo.Enabled = false;
+            this.txtDocenteCorreo.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.249999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDocenteCorreo.Location = new System.Drawing.Point(133, 461);
+            this.txtDocenteCorreo.Name = "txtDocenteCorreo";
+            this.txtDocenteCorreo.Size = new System.Drawing.Size(180, 20);
+            this.txtDocenteCorreo.TabIndex = 144;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(69, 343);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 16);
+            this.label3.TabIndex = 143;
+            this.label3.Text = "Nombre:";
+            // 
+            // txtDocenteNombre
+            // 
+            this.txtDocenteNombre.Enabled = false;
+            this.txtDocenteNombre.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.249999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDocenteNombre.Location = new System.Drawing.Point(133, 342);
+            this.txtDocenteNombre.Name = "txtDocenteNombre";
+            this.txtDocenteNombre.Size = new System.Drawing.Size(180, 20);
+            this.txtDocenteNombre.TabIndex = 142;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(68, 384);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(60, 16);
+            this.label4.TabIndex = 141;
+            this.label4.Text = "Apellido:";
+            // 
+            // txtDocenteApellido
+            // 
+            this.txtDocenteApellido.Enabled = false;
+            this.txtDocenteApellido.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.249999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDocenteApellido.Location = new System.Drawing.Point(133, 383);
+            this.txtDocenteApellido.Name = "txtDocenteApellido";
+            this.txtDocenteApellido.Size = new System.Drawing.Size(180, 20);
+            this.txtDocenteApellido.TabIndex = 140;
+            // 
+            // labelIIngresarValores
+            // 
+            this.labelIIngresarValores.AutoSize = true;
+            this.labelIIngresarValores.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelIIngresarValores.Location = new System.Drawing.Point(25, 300);
+            this.labelIIngresarValores.Name = "labelIIngresarValores";
+            this.labelIIngresarValores.Size = new System.Drawing.Size(198, 16);
+            this.labelIIngresarValores.TabIndex = 139;
+            this.labelIIngresarValores.Text = "Resultado de la búsqueda:";
+            // 
+            // botonOpciones
+            // 
+            this.botonOpciones.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.botonOpciones.Location = new System.Drawing.Point(293, 46);
+            this.botonOpciones.Name = "botonOpciones";
+            this.botonOpciones.Size = new System.Drawing.Size(23, 24);
+            this.botonOpciones.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.botonOpciones.TabIndex = 2;
+            this.botonOpciones.TabStop = false;
+            // 
+            // pbRegresar
+            // 
+            this.pbRegresar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pbRegresar.Image = global::UTApp.Properties.Resources.left_chevron;
+            this.pbRegresar.Location = new System.Drawing.Point(25, 30);
+            this.pbRegresar.Name = "pbRegresar";
+            this.pbRegresar.Size = new System.Drawing.Size(25, 40);
+            this.pbRegresar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbRegresar.TabIndex = 1;
+            this.pbRegresar.TabStop = false;
+            // 
+            // FormDocentesBuscar
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(344, 601);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtDocentePass);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtDocenteCorreo);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtDocenteNombre);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtDocenteApellido);
+            this.Controls.Add(this.labelIIngresarValores);
+            this.Controls.Add(this.btnBuscar);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtDocenteEmpleado);
+            this.Controls.Add(this.labelCatálogoMaterias);
+            this.Controls.Add(this.panelHeader);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FormDocentesBuscar";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "FormDocentesBuscar";
+            this.panelHeader.ResumeLayout(false);
+            this.panelHeader.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.botonOpciones)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbRegresar)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelHeader;
+        private System.Windows.Forms.PictureBox botonOpciones;
+        private System.Windows.Forms.PictureBox pbRegresar;
+        private System.Windows.Forms.Label labelMaterias;
+        private System.Windows.Forms.Label labelCatálogoMaterias;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtDocenteEmpleado;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtDocentePass;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtDocenteCorreo;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtDocenteNombre;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtDocenteApellido;
+        private System.Windows.Forms.Label labelIIngresarValores;
+    }
+}

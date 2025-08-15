76e953a63e314f6b9091c542938648bb8bb18ba4
eduuHT <eduuu.ht@gmail.com>
2025-08-14 17:44:29 -0700

    Add connection string

---
+++ UTApp/Connection.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UTApp
+{
+    internal class Connection
+    {
+        public static string connectionString = "workstation id=UTApp_Integradora1.mssql.somee.com;packet size=4096;user id=LuisRomán123_SQLLogin_1;pwd=b7jxk7yxyk;data source=UTApp_Integradora1.mssql.somee.com;persist security info=False;initial catalog=UTApp_Integradora1;TrustServerCertificate=True";
+    }
+}
